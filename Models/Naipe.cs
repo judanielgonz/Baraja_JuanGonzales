3c0ddeffc50e67b32a63f2a9588202dd1f06fcff
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-19 20:44:37 -0400

    Cree el proyecto y el modelo

---
+++ Models/Naipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baraja_JuanGonzales.Models
+{
+    public class Naipe
+    {
+        [Key]
+        public string NaipeId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        public string Nombre { get; set; }
+      
+        [Url]
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string ImagenUrl { get; set; }
+
+    }
+}

d429a3627f73f669d43757e3f96a2d4ef1f9aadd
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-19 20:55:05 -0400

    Agrege el archivo DbContext al archivo Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using Baraja_JuanGonzales.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,11 @@ namespace Baraja_JuanGonzales
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // DbContext
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
+
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>

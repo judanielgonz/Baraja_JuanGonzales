5dcbd2769772100d12a29bb3562165a4d8528837
Juan daniel Gonzales Aramayo <a2020116081@estudiantes.upsa.edu.bo>
2021-10-19 20:53:35 -0400

    Cree el directorio Data y la clase AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Baraja_JuanGonzales.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baraja_JuanGonzales.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Naipe> Naipe { get; set; }
+    }
+
+}
